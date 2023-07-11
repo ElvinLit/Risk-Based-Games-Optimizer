0779efc29ecf29c7ecba032d6c37ee125207e4a0
unknown <nicholastran1501@gmail.com>
2023-07-10 22:22:47 -0500

    Adding the optional target balance parameter

--- pages/reverse_martingale.py
+++ pages/reverse_martingale.py
@@ -16,24 +16,28 @@ add_page_title()
 # ----- BACKEND ----- #
 
 # Reverse Martingale implementation
-def reverse_martingale(initial_balance, num_plays, initial_bet, preference):
+def reverse_martingale(initial_balance, num_plays, initial_bet, preference, target_balance=None):
     """
     Simulates the martingale strategy returning balance on roulette given an initial balance, number of plays, initial bet, and color preference  
+    Stops betting if balance reaches or exceeds target_balance.
 
     Args:
-        starting_balance (int or float): Starting amount
+        initial_balance (int or float): Starting amount
         num_plays (int): Number of plays
         initial_bet (int or float): Starting amount
-        preference (string): Color preference from "Red", "Black", or "Green"
+        preference (string): Color preference from "red", "black", or "green"
+        target_balance (int or float, optional): Target amount
     Returns:
         int or float: end amount
     """
-    choices = ['Red', 'Black', 'Green']
+    choices = ['red', 'black', 'green']
     weights = [18/38, 18/38, 2/38]
     
     balance = initial_balance
     bet = initial_bet
     for _ in range(num_plays):
+        if balance <= bet or (target_balance is not None and balance >= target_balance):
+            break
         outcome = random.choices(choices, weights)[0]
         if outcome == preference:
             balance += bet
@@ -46,16 +50,6 @@ def reverse_martingale(initial_balance, num_plays, initial_bet, preference):
 
 # ----- FRONTEND ----- #
 
-# Description of the strategy
-st.write("The **reverse martingale system** is the reverse of the Martingale strategy. \
-         It is more attractive to traders than its counterpart due to the nature of the momentus markets, \
-         but this strategy's origin was for betting applications. It is an algorithm with the goal of maximizing \
-         profits on winning streaks and minimizing the losses on losing streaks.")
-st.write("The strategy can be algorithmically described simply as follows: ")
-st.text("1. Set an initial bet")
-st.text("2. If you win a bet, reset the bet to the initial bet and continue.")
-st.text("3. If you lose a bet, double the bet for the next round.")
-
 # Handles form data
 st.subheader("Visualize based on your parameters")
 
@@ -63,15 +57,13 @@ with st.form(key='reverse_martingale_parameters'):
     initial_balance = st.number_input("Initial Balance")
     num_plays = int(st.number_input("Number of Plays"))
     initial_bet = st.number_input("Initial Bet")
-    preference = st.text_input("Color (choose from 'Red', 'Black', or 'Green')")
+    preference = (st.text_input("Color (choose from 'Red', 'Black', or 'Green')")).lower()
     repeats =  int(st.number_input("Sample repetitions"))
-    
+    target_balance = st.number_input("Target Balance", min_value=0.0, value=None, step=0.1, help="Optional: Betting stops once the balance has reached or exceeds this value. Leave blank for no target.") # new target_balance field
+
     submit_button = st.form_submit_button(label='Visualize')
 
 if submit_button:
-    samples = sample(reverse_martingale, repeats, initial_balance, num_plays, initial_bet, preference)
+    samples = sample(reverse_martingale, repeats, initial_balance, num_plays, initial_bet, preference, target_balance if target_balance > 0 else None)
     martingale_df = dataframe_conversion(samples)
     frequency_plot(martingale_df, initial_balance)
-
-
-
