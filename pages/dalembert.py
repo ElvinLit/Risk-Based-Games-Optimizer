c1ede778e83b1edd5aec4df8a9193b9eda872cd8
Zakaria Khan <zakaria.kahn04@gmail.com>
2023-07-14 23:56:08 -0500

    Realigned text and buttons

--- pages/dalembert.py
+++ pages/dalembert.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from st_pages import Page, show_pages, add_page_title
 import random
-from packages.graphs import frequency_plot, line_plot, roulette_plot
+from packages.graphs import frequency_plot, line_plot, box_plot, roulette_plot
 from packages.data_manipulation import sample, dataframe_conversion
 
 # Setting page configuration
@@ -50,25 +50,44 @@ def dalembert(initial_balance, num_plays, base_bet, preference):
 
 
 # ----- FRONTEND ----- #
+# Setting columns
+col1, col2 = st.columns([1,1])
 
-# Description of the strategy
-st.write("The **D'Alembert system** is a betting strategy used for Roulette. It is an algorithm with the goal of playing conservatively. The allure of this strategy surrounds the superficial ease of a balanced recovery after loss.")
-st.write("The algorithm for this strategy involves the following guidelines: ")
-st.text("1. Set an base bet")
-st.text("2. If you win a bet, subtract the bet by the base bet and continue.")
-st.text("3. If you lose a bet, add the base bet for the next round.")
+with col1:
+    # Description of the strategy
+    st.write("The **D'Alembert system** is a betting strategy used for Roulette. It is an algorithm with the goal of playing conservatively. The allure of this strategy surrounds the superficial ease of a balanced recovery after loss.")
+    st.write("The algorithm for this strategy involves the following guidelines: ")
+    st.text("1. Set an base bet")
+    st.text("2. If you win a bet, subtract the bet by the base bet and continue.")
+    st.text("3. If you lose a bet, add the base bet for the next round.")
 
-# Handles form data
-st.subheader("Visualize based on your parameters")
+    # Handles form data
 
-with st.form(key='dalembert_parameters'):
-    initial_balance_text = st.text_input("Initial Balance", placeholder= '200')
-    num_plays_text = st.text_input("Number of Plays", placeholder= '10')
-    initial_bet_text = st.text_input("Initial Bet", placeholder= '10')
-    preference = (st.selectbox("Color", options=['Red', 'Black', 'Green'])).lower()
-    repeats_text =  st.text_input("Sample repetitions", placeholder= '100')
-    target_balance_text = st.text_input("Target Balance", placeholder = "None", help="Optional: Betting stops once the balance has reached or exceeds this value. Leave None for no target.")
-    submit_button = st.form_submit_button(label='Visualize')
+# Subheader 
+st.markdown(
+    """
+    <style>
+    .custom-subheader {
+        text-align: center; /* Change the text alignment to left */
+        font-family: Arial
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,)
+st.markdown('<h2 class="custom-subheader">Visualize Based on your Parameters</h2>', unsafe_allow_html=True)
+
+with col2:
+    with st.form(key='dalembert_parameters'):
+        # Argument entries
+        initial_balance_text = st.text_input("Initial Balance", placeholder= '200')
+        num_plays_text = st.text_input("Number of Plays", placeholder= '10')
+        initial_bet_text = st.text_input("Initial Bet", placeholder= '10')
+        preference = (st.selectbox("Color", options=['Red', 'Black', 'Green'])).lower()
+        repeats_text =  st.text_input("Sample repetitions", placeholder= '100')
+        target_balance_text = st.text_input("Target Balance", placeholder = "None", help="Optional: Betting stops once the balance has reached or exceeds this value. Leave None for no target.")
+
+        # Submit button
+        submit_button = st.form_submit_button(label='Visualize')
 
 if submit_button:
     try:
@@ -87,4 +106,4 @@ if submit_button:
 
     samples = sample(dalembert, repeats, initial_balance, num_plays, base_bet, preference, target_balance if target_balance > 0 else None)
     dalembert_df = dataframe_conversion(samples)
-    roulette_plot(line_plot(dalembert, num_plays, initial_balance, base_bet, preference), frequency_plot(dalembert_df, initial_balance, repeats, graph_width))
+    roulette_plot(line_plot(dalembert, num_plays, initial_balance, base_bet, preference), frequency_plot(dalembert_df, initial_balance, repeats, graph_width), box_plot(dalembert_df, initial_balance, repeats, graph_width))
