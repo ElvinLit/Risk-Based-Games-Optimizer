56d51ba8ee40e2189485cf037613e340d5f6822c
Elvin Li <li.elvin739@gmail.com>
2023-07-21 02:02:34 -0500

    Updated about section

--- pages/roulette_explorer.py
+++ pages/roulette_explorer.py
@@ -28,7 +28,8 @@ st.markdown(
 st.markdown('<h2 class="custom-subheader">Roulette Overview</h2>', unsafe_allow_html=True)
 
 st.write("*Roulette* is a popular casino game where players bet on a color, number, or various combinations of the two. For the purposes of our page, \
-        we allow users to only select colors as they represent the highest chances of winning: **18/38** for Red or Black, **2/38** for Green. From this, \
+        we allow users to only select colors as they represent the highest chances of winning: **18/38** for Red or Black, **2/38** for Green. On a winning \
+        bet, players win twice their bet amount, essentially net gaining the amount that they bet. From observing the possibilities, \
         it is evident that even the best chance of winning is less than 50%. The Expected Value (EV) can be calculated statistically. Let P be \
         the probability of winning, Q=1-P be the probability of losing, and a bet of $10. Thus follows: ")
 st.latex(r'''
@@ -52,4 +53,6 @@ st.latex(r'''
 \end{align*}
     ''')
 st.write("As you can see, given an initial bet of 10 dollars, we are expected to lose around 1.58 dollars on every round that we play. However, we have different betting \
-            systems that you are free to play around with to try and find a 'winning' strategy. On this page, you can compare the graphs for these strategies based on your parameters.")
+        systems that you are free to play around with to try and find a 'winning' strategy. Keep in mind that not every strategy is foolproof, and especially for roulette, \
+        it is almost nearly impossible to find a completely winning strategy. These visualizations are meant to serve as fun observations, not guidelines for your \
+        gambling endeavors.")
