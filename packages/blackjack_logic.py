f57a144c13fe2a959b104fbc70684c67d927729a
Elvin Li <li.elvin739@gmail.com>
2023-08-08 20:57:55 -0500

    Created counting strategies for blackjack

--- packages/blackjack_logic.py
+++ packages/blackjack_logic.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Card:
     def __init__(self, value):
         self.value = value
@@ -15,6 +16,7 @@ class Card:
     def __str__(self):
         return self.value
 
+
 class Deck:
     def __init__(self):
         self.cards = [Card(val) for val in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']] * 4
@@ -25,6 +27,7 @@ class Deck:
     def show_deck(self):
         return ', '.join(str(card) for card in self.cards)
     
+
 class Hand:
     def __init__(self):
         self.cards = []
@@ -42,3 +45,48 @@ class Hand:
 
     def __str__(self):
         return ', '.join([card.value for card in self.cards])
+
+
+class CardCounter:
+    def __init__(self):
+        self.running_count = 0
+
+    def high_low(self, card):
+        value = card.get_value()
+        if value in [2, 3, 4, 5, 6]:
+            self.running_count += 1
+        elif value in [10, 11]: # '10', 'J', 'Q', 'K', 'A'
+            self.running_count -= 1
+
+    def halves(self, card):
+        value = card.get_value()
+        if value == 2:
+            self.running_count += 0.5
+        elif value == 3 or value == 4 or value == 6:
+            self.running_count += 1
+        elif value == 5:
+            self.running_count += 1.5
+        elif value == 7:
+            self.running_count += 0.5
+        elif value == 9:
+            self.running_count -= 0.5
+        elif value in [10, 11]: # '10', 'J', 'Q', 'K', 'A'
+            self.running_count -= 1
+    
+    def zen(self, card):
+        value = card.get_value()
+        if value in [2, 3, 7]:
+            self.running_count += 1
+        elif value in [4, 5, 6]:
+            self.running_count += 2
+        elif value in [10, 11]: # '10', 'J', 'Q', 'K', 'A'
+            if card.value == 'A':
+                self.running_count -= 1
+            else:
+                self.running_count -= 2
+
+    def get_running_count(self):
+        return self.running_count
+
+    def reset_count(self):
+        self.running_count = 0
